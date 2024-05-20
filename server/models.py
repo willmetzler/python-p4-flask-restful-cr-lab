0545c32985252d75cb66951c98295038b9ed6c13
Will Metzler <metzlerwill@gmail.com>
2024-05-20 11:33:27 -0400

    finish lab

--- server/models.py
+++ server/models.py
@@ -7,3 +7,14 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price,
+        }
